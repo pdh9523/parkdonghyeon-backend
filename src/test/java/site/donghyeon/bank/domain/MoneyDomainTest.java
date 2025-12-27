b7e915872b46895991d83c61a1cb4cb41bda2a60
박동현 <jinee9523@gmail.com>
2025-12-27 21:40:57 +0900

    test: 금액 도메인 테스트

--- src/test/java/site/donghyeon/bank/domain/MoneyDomainTest.java
+++ src/test/java/site/donghyeon/bank/domain/MoneyDomainTest.java
@@ -1,11 +1,9 @@
 package site.donghyeon.bank.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 import site.donghyeon.bank.common.domain.Money;
-import site.donghyeon.bank.common.exception.NegativeAmountException;
 
 class MoneyDomainTest {
 
@@ -42,8 +40,38 @@ class MoneyDomainTest {
     }
 
     @Test
-    void 금액은_0미만이_될_수_없다() {
-        assertThatThrownBy(() -> new Money(-1L))
-                .isInstanceOf(NegativeAmountException.class);
+    void 음수_전환_테스트() {
+        Money money = new Money(1000);
+        Money minus = money.toMinus();
+
+        assertThat(minus.amount()).isEqualTo(-1000);
+    }
+
+    @Test
+    void 초과_여부_테스트() {
+        Money money1 = new Money(1);
+        Money money2 = new Money(2);
+        Money money3 = new Money(2);
+
+        assertThat(money1.exceeded(money2)).isFalse();
+        assertThat(money2.exceeded(money3)).isFalse();
+        assertThat(money3.exceeded(money1)).isTrue();
+    }
+
+    @Test
+    void 수수료_테스트() {
+        Money money = new Money(1000);
+        Money fee = money.getFee();
+        Money withFee = money.withFee();
+
+        assertThat(fee.amount()).isEqualTo(10);
+        assertThat(withFee.amount()).isEqualTo(1010);
+
+        Money small = new Money(1);
+        Money smallFee = small.getFee();
+        Money smallWithFee = small.withFee();
+
+        assertThat(smallFee.amount()).isEqualTo(0);
+        assertThat(smallWithFee.amount()).isEqualTo(1);
     }
 }
