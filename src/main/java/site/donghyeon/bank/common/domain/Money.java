369b2d04672735dac176dde5d26e92052a0e970c
박동현 <jinee9523@gmail.com>
2025-12-27 20:55:26 +0900

    refactor: 금액 도메인 제약사항 변경

--- src/main/java/site/donghyeon/bank/common/domain/Money.java
+++ src/main/java/site/donghyeon/bank/common/domain/Money.java
@@ -1,13 +1,6 @@
 package site.donghyeon.bank.common.domain;
 
-import site.donghyeon.bank.common.exception.NegativeAmountException;
-
 public record Money(long amount) {
-    public Money {
-        if (amount < 0) {
-            throw new NegativeAmountException(amount);
-        }
-    }
 
     public static Money zero() {
         return new Money(0);
@@ -25,12 +18,16 @@ public record Money(long amount) {
         return this.amount > other.amount;
     }
 
+    public Money toMinus() {
+        return new Money(-this.amount);
+    }
+
     public Money getFee() {
-        return new Money((long) (this.amount * 0.1));
+        return new Money((long) (this.amount * 0.01));
     }
 
     public Money withFee() {
-        return new Money((long) (this.amount * 1.1));
+        return new Money((long) (this.amount * 1.01));
     }
 }
 
