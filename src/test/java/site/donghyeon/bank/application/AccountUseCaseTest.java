8103c927bc7db884ec3aea28d29d2f8f7aaf4b42
박동현 <jinee9523@gmail.com>
2025-12-29 23:12:08 +0900

    test: 계좌 유스케이스 테스트

--- src/test/java/site/donghyeon/bank/application/AccountUseCaseTest.java
+++ src/test/java/site/donghyeon/bank/application/AccountUseCaseTest.java
@@ -6,6 +6,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
@@ -14,10 +16,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import site.donghyeon.bank.application.account.exception.RemainingBalanceException;
+import site.donghyeon.bank.application.account.query.MyAccountsQuery;
+import site.donghyeon.bank.application.account.result.MyAccountsResult;
 import site.donghyeon.bank.application.account.service.AccountService;
 import site.donghyeon.bank.application.account.command.CloseAccountCommand;
 import site.donghyeon.bank.application.account.command.OpenAccountCommand;
 import site.donghyeon.bank.application.account.result.OpenAccountResult;
+import site.donghyeon.bank.application.account.view.AccountView;
 import site.donghyeon.bank.common.domain.Money;
 import site.donghyeon.bank.domain.account.Account;
 import site.donghyeon.bank.domain.account.enums.AccountStatus;
@@ -70,5 +75,45 @@ class AccountUseCaseTest {
                 .isInstanceOf(RemainingBalanceException.class);
 
         then(accountRepository).should(never()).save(any());
+    }
+
+    @Test
+    void 내_계좌_조회_테스트() {
+        // given
+        AccountView view = new AccountView(
+                TEST_ACCOUNT_ID,
+                new Money(1000),
+                Instant.parse("2025-01-01T00:00:00Z")
+        );
+
+        MyAccountsQuery query = new MyAccountsQuery(TEST_USER_ID);
+
+        when(accountRepository.findAllByUserId(TEST_USER_ID))
+                .thenReturn(List.of(view));
+
+        MyAccountsResult result = accountService.getMyAccounts(query);
+
+        assertThat(result.accounts()).hasSize(1);
+        assertThat(result.accounts().get(0)).isEqualTo(view);
+
+        then(accountRepository)
+                .should()
+                .findAllByUserId(TEST_USER_ID);
+    }
+
+    @Test
+    void 보유_계좌_없을_시_빈_리스트_반환() {
+        MyAccountsQuery query = new MyAccountsQuery(TEST_USER_ID);
+
+        when(accountRepository.findAllByUserId(TEST_USER_ID))
+                .thenReturn(List.of());
+
+        MyAccountsResult result = accountService.getMyAccounts(query);
+
+        assertThat(result.accounts()).hasSize(0);
+
+        then(accountRepository)
+                .should()
+                .findAllByUserId(TEST_USER_ID);
     }
 }
